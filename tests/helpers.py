4f52f55d7f8d052f7b5b461456e1e99ffff98e60
David Svenson <davidsvenson@outlook.com>
2025-12-03 10:07:53 +0100

    Fix ruff complaint.

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,7 +4,9 @@ import datetime
 from typing import Any
 from zoneinfo import ZoneInfo
 
+UTC = ZoneInfo("UTC")
 
-def datetime_utc(*args: Any, tzinfo: ZoneInfo = ZoneInfo("UTC"), **kwargs: Any) -> datetime.datetime:
+
+def datetime_utc(*args: Any, tzinfo: ZoneInfo = UTC, **kwargs: Any) -> datetime.datetime:
     # "datetime" gets multiple values for keyword argument "tzinfo"
     return datetime.datetime(*args, tzinfo=tzinfo, **kwargs)  # type: ignore[misc]
